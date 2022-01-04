423da8ee9774a5a70c9e26558ce05f8dc1de9ead
Jfisher37 <joseph.eric.fisher@gmail.com>
2022-01-04 12:54:53 -0500

    working on getting nav selector to work on tiny mode. Links are generating for reg nav items. Selector itself isn't working.

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ import about from "./pages/about.js";
 import aboutSmall from "./pages/aboutSmall.js";
 import work from "./pages/work.js";
 import contact from "./pages/contact.js";
+import navTiny from "./pages/navTiny.js";
 
 const navEl = document.getElementById("nav");
 const mainEl = document.getElementById("main");
 let linkEls = document.querySelectorAll(".nav-link");
+const navSmallSelectEl = document.getElementById("menu-select-s");
 
 let activeLink = "about";
+// let prevLink = "about";
 
-// const generateElements = () => {
-//     linkEls = document.querySelectorAll(".nav-link");
+// const generateTinyNav = () => {
+//     return navTiny();
+//     ;
 // }
 
 const mainBig = () => {
+
   switch (activeLink) {
     case "about":
       return about();
@@ -23,6 +28,11 @@ const mainBig = () => {
         return work();
     case "contact":
         return contact();
+    // case "nav-tiny":
+    //     return navTiny();
+    // case "close-nav-tiny":
+    //     activeLink = prevLink;
+    //     return mainBig();
     default:
       break;
   }
@@ -41,15 +51,61 @@ const mainSmall = () => {
   }
 };
 
-// const generateLinks = () => {
+const toggleTinyNavSelector = () => {
+    if (navSmallSelectEl.dataset.loc === "nav-tiny"){
+        navSmallSelectEl.removeAttribute("data-loc");
+        navSmallSelectEl.setAttribute("data-loc", "close-nav-tiny");
+    } else {
+        navSmallSelectEl.removeAttribute("data-loc");
+        navSmallSelectEl.setAttribute("data-loc", "nav-tiny");
+    }
+}
+
+const generateLinks = () => {
     linkEls.forEach((link) => {
-      link.addEventListener("click", (e) => {
+      link.addEventListener("click",  (e) => {
         e.preventDefault();
+        // console.log(activeLink);
+        // console.log(prevLink);
+        if (link.dataset.loc !== "close-nav-tiny" && link.dataset.loc !== "nav-tiny"){
+            console.log("reg")
         activeLink = link.dataset.loc;
         mainEl.innerHTML = mainBig();
+        toggleTinyNavSelector();
+    };
+    if (link.dataset.loc === "close-nav-tiny"){
+        console.log("close")
+        mainEl.innerHTML = mainBig();
+        toggleTinyNavSelector();
+    } if (link.dataset.loc === "nav-tiny"){
+        console.log("nav")
+        mainEl.innerHTML = navTiny()
+        toggleTinyNavSelector();
+        linkEls = document.querySelectorAll(".nav-link");
+        generateLinks();
+    }
+        // console.log(activeLink);
+        // console.log(prevLink);
+        
+        // mainEl.innerHTML = await mainBig();
+        // if (activeLink === "nav-tiny"){
+        //     linkEls = document.querySelectorAll(".nav-link");
+        //     generateLinks();
+        //     console.log(activeLink);
+        //     activeLink = prevLink;
+        //     console.log(activeLink);
+        // }
       });
     });
-//   };
+  };
+
+//   linkEls.forEach((link) => {
+//       link.addEventListener("click", (e) => {
+//     e.preventDefault();
+    
+// })
+//   });
+
 
 // const sizeChecker = () => {
 //   const winWidth = window.innerWidth;
@@ -71,6 +127,7 @@ const mainSmall = () => {
 
 window.onload = () => {
     mainEl.innerHTML = mainBig();
+    generateLinks();
 };
 
 // window.onresize = () => {
---
+++ pages/navTiny.js
@@ -0,0 +1,21 @@
+const navTiny = () => {
+    return `      
+    <ul id="nav-tiny">
+        <li class="nav-link" data-loc="about" tabindex="0">
+            About Me
+        </li>
+        <li class="nav-link" data-loc="work" tabindex="0">
+            My Work
+        </li>
+        <li class="nav-link" data-loc="contact" tabindex="0">
+            Contact Me
+        </li>
+        <li class="nav-link"><a class="inactive" href="#" tabindex="0">
+            Resume
+        </a></li>
+    </ul>
+  `;
+  };
+  
+  export default navTiny;
+
