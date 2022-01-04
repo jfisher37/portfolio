2288c70c87b06943f6f772c7550622738a996970
Jfisher37 <joseph.eric.fisher@gmail.com>
2022-01-04 17:34:57 -0500

    got the active/inactive class to toggle with big nav. Still strullging with little nav.

--- index.js
+++ index.js
@@ -9,16 +9,11 @@ import navTiny from "./pages/navTiny.js";
 const navEl = document.getElementById("nav");
 const mainEl = document.getElementById("main");
 let linkEls = document.querySelectorAll(".nav-link");
-const navSmallSelectEl = document.getElementById("menu-select-s");
+let navSmallSelectEl = document.getElementById("menu-select-s");
+
 
 let activeLink = "about";
-// let prevLink = "about";
-let initWidth =  window.innerWidth;
-
-// const generateTinyNav = () => {
-//     return navTiny();
-//     ;
-// }
+let initWidth = window.innerWidth;
 
 const mainBig = () => {
   switch (activeLink) {
@@ -28,126 +23,81 @@ const mainBig = () => {
       return work();
     case "contact":
       return contact();
-    // case "nav-tiny":
-    //     return navTiny();
-    // case "close-nav-tiny":
-    //     activeLink = prevLink;
-    //     return mainBig();
     default:
       break;
-  }
-};
-
-const mainSmall = () => {
-  switch (activeLink) {
-    case "about":
-      return aboutSmall();
-    // case "work":
-    //     return workSmall();
-    // case "contact":
-    // return contactSmall();
-    default:
-      break;
-  }
-};
-
-const toggleTinyNavSelector = () => {
-  if (navSmallSelectEl.dataset.loc === "nav-tiny") {
-    // navSmallSelectEl.removeAttribute("data-loc");
-    navSmallSelectEl.setAttribute("data-loc", "close-nav-tiny");
-    console.log("SET CLOSE")
-  } else {
-    // navSmallSelectEl.removeAttribute("data-loc");
-    navSmallSelectEl.setAttribute("data-loc", "nav-tiny");
-    console.log("SET OPEN");
   }
 };
 
 const linksForBig = () => {
-    linkEls.forEach((link) => {
-        link.addEventListener("click", (e) => {
-          e.preventDefault();
-  activeLink = link.dataset.loc;
-  mainEl.innerHTML = mainBig();
-  if (activeLink === "nav-tiny") {
-    linkEls = document.querySelectorAll(".nav-link");
-    generateLinks();
-    activeLink = "";
-  }
-  console.log(linkEls);
-});
-    })};
+  linkEls.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      activeLink = link.dataset.loc;
+      linkEls.forEach((link) => {
+          link.setAttribute("class", "nav-link inactive")
+      });
+      link.setAttribute("class", "nav-link active");
+      mainEl.innerHTML = mainBig();
+    });
+  });
+};
+
+const toggleTinyNavSelector = () => {
+    const navTinyEl = document.getElementById("nav-tiny");
+    console.log("HERE!")
+  if (navSmallSelectEl.dataset.loc === "nav-tiny" && navTinyEl) {
+    navSmallSelectEl.setAttribute("data-loc", "close-nav-tiny");
+    console.log("SET CLOSE");
+  } else if (navSmallSelectEl.dataset.loc === "close-nav-tiny" && !navTinyEl){
+    navSmallSelectEl.setAttribute("data-loc", "nav-tiny");
+    console.log("SET OPEN");
+  } else {console.log("WHAT IS HAPPENING?")}
+//   else {
+//       console.log("NOW WHAT");
+//     const navTinyEl = document.getElementById("nav-tiny");
+//     if (navTinyEl) {
+//         navSmallSelectEl.setAttribute("data-loc", "close-nav-tiny");
+//     } else {
+//         navSmallSelectEl.setAttribute("data-loc", "nav-tiny");
+//     }
+//   }
+};
+
 
 const linksForSmall = () => {
-    linkEls.forEach((link) => {
-        link.addEventListener("click", (e) => {
-          e.preventDefault();
-    console.log(link);
-    const loc = link.dataset.loc;
-    console.log(loc);
-    // if (loc !== loca){
-    //     console.log(loc);
-    //     console.log(loca);
-    //     return;
-    // }
-  if (
-    loc !== "close-nav-tiny" &&
-    loc !== "nav-tiny"
-  ) {
-    console.log("reg");
-    activeLink = link.dataset.loc;
-    mainEl.innerHTML = mainBig();
-    toggleTinyNavSelector();
-  }
-  if (loc === "close-nav-tiny") {
-    console.log("close");
-    mainEl.innerHTML = mainBig();
-    toggleTinyNavSelector();
-  }
-  if (loc === "nav-tiny") {
-    console.log("nav");
-    mainEl.innerHTML = navTiny();
-    toggleTinyNavSelector();
-    linkEls = document.querySelectorAll(".nav-link");
-    generateLinks();
-  }
-});
-});
+  linkEls.forEach((link) => {
+    link.addEventListener("click",  (e) => {
+      e.preventDefault();
+       console.log(link);
+      console.log(link.dataset.loc);
+      const loc = link.dataset.loc;
+    //   navSmallSelectEl.removeAttribute("data-loc");
+      console.log(loc);
+      if (loc !== "close-nav-tiny" && loc !== "nav-tiny") {
+        activeLink = link.dataset.loc;
+        mainEl.innerHTML = mainBig();
+      }
+      if (loc === "close-nav-tiny") {
+        mainEl.innerHTML = mainBig();
+      }
+      if (loc === "nav-tiny") {
+        mainEl.innerHTML = navTiny();
+        linkEls = document.querySelectorAll(".nav-link");
+        linksForSmall();
+    }
+      toggleTinyNavSelector();
+    });
+  });
 };
 
 const generateLinks = () => {
   const winWidth = window.innerWidth;
-      if (winWidth <= 790) {
-          console.log("SMALL!!!!")
-        linksForSmall();
-      } else {
-          console.log("BIG!!")
-        linksForBig();
-      }
-    }
-
-//   linkEls.forEach((link) => {
-//       link.addEventListener("click", (e) => {
-//     e.preventDefault();
-
-// })
-//   });
-
-// const sizeChecker = () => {
-//   const winWidth = window.innerWidth;
-
-//   if (winWidth <= 790) {
-//     navEl.innerHTML = navSmall();
-//     mainEl.innerHTML = mainSmall();
-//     generateElements();
-//     generateLinks();
-//   } else {
-//     navEl.innerHTML = nav();
-//     mainEl.innerHTML = mainBig();
-//     generateElements();
-//     generateLinks();
-//   }
-// };
+  if (winWidth <= 790) {
+    linksForSmall();
+  } else {
+    linksForBig();
+  }
+};
 
 window.onload = () => {
   mainEl.innerHTML = mainBig();
@@ -158,7 +108,7 @@ window.onresize = () => {
   const initDiff = initWidth - 790;
   const winWidth = window.innerWidth;
   const currentDiff = winWidth - 790;
-  if (initDiff*currentDiff < 0) {
+  if (initDiff * currentDiff < 0) {
     initWidth = window.innerWidth;
     generateLinks();
   }
