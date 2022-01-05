8ba62be422977559c789b64c84956438b65ab38a
Jfisher37 <joseph.eric.fisher@gmail.com>
2022-01-04 23:05:51 -0500

    migrated about page into appropriate js file.

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,16 @@
 const about = () => {
     return `
-    <p>ABOUT ME</p>
+    <div id="about-page">
+    <img id="joey-photo" alt="A photo of me" src="assets/images/placeholder_300x300.jpeg">
+    <section id="about-section">
+        <p2 id="about-label">
+            About Me
+        </p2>
+        <p id="about-text">
+            I am an aspiring web developer, currently studying at Penn LPS's Bootcamp. My background is in education. I have my M.S.Ed from UPenn, with a specialization in second language acquisition. So, as you might have guessed, I'm a huge language nerd - I'm fluent in Spanish, Portuguese, Japanese, HTML, CSS, and Javascript. Oh, and English too, I guess. 
+        </p>
+    </section>
+  </div>
     `
 }
 
